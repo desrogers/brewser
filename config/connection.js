6f7809f737e5e4b6cd6d9e2ced8ac00cfc1f1b63
dezroge <desmondrogers2@gmail.com>
2019-04-13 02:47:35 -0500

    hooking with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "brews_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'brews_db'
+  })
+}
+
 // Connect to the database
 connection.connect(function (err) {
   if (err) {
